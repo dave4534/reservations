48b6fba655d37caeb4a7e66c346152f05575ae5e
uDave <uDave@Dave.local>
2016-11-03 21:22:08 +0200

    Dave's commit

--- main.js
+++ main.js
@@ -1,12 +1,26 @@
 var reservations = {
   'Bob': { claimed: false },
   'Ted': { claimed: true }
-}
+};
 
 var name = prompt('Please enter the name for your reservation');
 
-var claimReservation = function () {
-  // write your code here!
-}
+var toTitleCase = function(name){
+  return name[0].toUpperCase() + name.slice(1).toLowerCase();
+};
+name = toTitleCase(name);
+
+var claimReservation = function() {
+
+  if (reservations[name] && reservations[name].claimed === false){
+    alert("Welcome " + name);
+  } else if (reservations[name] && reservations[name].claimed){
+    alert("You are already checked in!");
+  } else {
+    reservations[name] = name;
+    reservations[name].claimed = true;
+    alert("There is nothing under that name, but a new reservation has been added!");
+  }
+};
 
 claimReservation();
